0b9e2e8d67a9fff566b61b6b23183d2274cef05d
vpub <valsy@mail.ru>
2019-09-22 20:36:53 +0300

    Added the name of sender and text "you sent this message:"

--- testbot-001.py
+++ testbot-001.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.chat.first_name + ', you sent this message: ' + message.text)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
