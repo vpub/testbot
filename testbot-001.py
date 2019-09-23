84cd6fab315fb088137b5bc59c34807db267e8e1
vpub <valsy@mail.ru>
2019-09-23 18:19:55 +0300

    Added welcome message to 'start' command.

--- testbot-001.py
+++ testbot-001.py
@@ -3,6 +3,14 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=['start'])
+def send_welcome_for_start(message):
+	bot.send_message(message.chat.id, "G'day! How are you doing? I can do all the things in this world. Just write down what you want.")
+
+@bot.message_handler(commands=['help'])
+def send_help_message(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.chat.first_name + ', you sent this message: ' + message.text)
