ee810e7c86bb648a43524dbc9386a19cd353bfa0
vpub <valsy@mail.ru>
2019-09-23 18:26:58 +0300

    1. Added help message to 'help' command. 2. Added personal greetings by name to 'start' command.

--- testbot-001.py
+++ testbot-001.py
@@ -5,11 +5,11 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome_for_start(message):
-	bot.send_message(message.chat.id, "G'day! How are you doing? I can do all the things in this world. Just write down what you want.")
+	bot.send_message(message.chat.id, "G'day, " + message.chat.first_name + "! How are you doing? I can do all the things in this world. Just write down what you want.")
 
 @bot.message_handler(commands=['help'])
 def send_help_message(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.send_message(message.chat.id, "If you want to do 1, just do 1. \nIf you want to do 2, just do 2.")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
